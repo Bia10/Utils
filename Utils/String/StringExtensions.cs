cc82ace3ccd261d3bb0832abb3634291693735b1
Bia10 <t.locke1234@gmail.com>
2021-05-04 15:01:31 +0200

    Add project files.

---
+++ Utils/String/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Utils.String
+{
+    public static class StringExtensions
+    {
+        public static bool IsDigitOnly(this string str)
+        {
+            return str.All(char.IsDigit);
+        }
+
+        public static string StringBetweenStrings(this string input, string start, string end)
+        {
+            var indexOfStart = input.IndexOf(start, StringComparison.Ordinal) + start.Length;
+            var indexOfEnd = input.IndexOf(end, indexOfStart, StringComparison.Ordinal);
+
+            return string.IsNullOrEmpty(end) ? input[indexOfStart..] : input[indexOfStart..indexOfEnd];
+        }
+
+        public static bool Contains(this string source, string toCheck, StringComparison comp)
+        {
+            return source?.IndexOf(toCheck, comp) >= 0;
+        }
+    }
+}
