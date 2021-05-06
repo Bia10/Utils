19e0161c59bc9a4230a8f93293bb3fd726c786ff
Bia10 <t.locke1234@gmail.com>
2021-05-06 13:28:55 +0200

    add process launcher

---
+++ Utils/Process/CmdArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Utils.Process
+{
+    public struct CmdArgs
+    {
+        public struct Steam
+        {
+            public const string AnonLogin = "+login anonymous";
+            public const string ForceInstallDir = " +force_install_dir ";
+            public const string DownloadWorkshopItem = " +workshop_download_item";
+            public const string RimAppId = " 294100 ";
+            public const string Quit = " +quit";
+        }
+    }
+}
---
+++ Utils/Process/Launcher.cs
@@ -0,0 +1,68 @@
+﻿using Spectre.Console;
+using System;
+using System.Diagnostics;
+using System.IO;
+using Utils.Console;
+
+namespace Utils.Process
+{
+    public class Launcher
+    {
+        private static string _exePath;
+        private readonly string _cmdArgs;
+        public bool Finished { get; private set; }
+
+        public Launcher(string exePath, string cmdArgs)
+        {
+            _exePath = exePath;
+            _cmdArgs = cmdArgs;
+        }
+
+        public void Launch()
+        {
+            var exeFile = new FileInfo(_exePath);
+            var workDir = exeFile.DirectoryName;
+            if (workDir == null)
+                throw new InvalidOperationException("Work directory null, cannot launch!");
+
+            try
+            {
+                var process = new System.Diagnostics.Process
+                {
+                    StartInfo =
+                    {
+                        FileName = _exePath,
+                        Arguments = _cmdArgs,
+                        WorkingDirectory = workDir,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
+                    }
+                };
+
+                process.OutputDataReceived += OutputHandler;
+                process.ErrorDataReceived += ErrorHandler;
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+
+                if (process.HasExited) Finished = true;
+            }
+            catch (Exception ex)
+            {
+                AnsiConsole.WriteException(ex);
+            }
+        }
+
+        private static void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            Extensions.Log(outLine.Data, "info");
+        }
+
+        private static void ErrorHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            Extensions.Log(outLine.Data, "error");
+        }
+    }
+}
