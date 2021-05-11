71d0f67fe545da7700bae457c5e280c5e7f5d5a2
Bia10 <t.locke1234@gmail.com>
2021-05-11 20:18:28 +0200

    add methods, rewrite To<T> method

--- src/Utils/String/StringExtensions.cs
+++ src/Utils/String/StringExtensions.cs
@@ -27,26 +27,32 @@ namespace Utils.String
             return new string(str.Where(char.IsDigit).ToArray());
         }
 
-        public static T To<T>(this string str)  //TODO: validation
+        public static bool ContainsSpecialDigitChar(this string str)
         {
             if (!str.Valid())
                 throw new InvalidOperationException("Input string in wrong format!");
 
-            return typeof(T) switch
-            {
-                { } intType when intType == typeof(int) =>
-                    (T) Convert.ChangeType(int.Parse(str), typeof(T)),
-                { } floatType when floatType == typeof(float) => 
-                    (T) Convert.ChangeType(float.Parse(str), typeof(T)),
-                { } doubleType when doubleType == typeof(double) =>
-                    (T) Convert.ChangeType(double.Parse(str), typeof(T)),
-                { } decimalType when decimalType == typeof(decimal) =>
-                    (T) Convert.ChangeType(decimal.Parse(str), typeof(T)),
-                { } longType when longType == typeof(long) => 
-                    (T) Convert.ChangeType(long.Parse(str), typeof(T)),
+            var specialChars = new string[] {"-", ".", "e", "E"};
 
-                _ => throw new InvalidOperationException("Input type is not supported!")
-            };
+            return str.ContainsAny(specialChars);
+        }
+
+        public static bool Contains(this string str, string toCheck, StringComparison comparison)
+        {
+            if (!str.Valid())
+                throw new InvalidOperationException("Input string in wrong format!");
+
+            return str.IndexOf(toCheck, comparison) >= 0;
+        }
+
+        public static bool ContainsAny(this string str, params string[] strings)
+        {
+            if (!str.Valid())
+                throw new InvalidOperationException("Input string in wrong format!");
+            if (!strings.Any())
+                throw new InvalidOperationException("Input params empty!");
+
+            return strings.Any(str.Contains); 
         }
 
         public static string StringBetweenStrings(this string str, string start, string end)
@@ -60,12 +66,106 @@ namespace Utils.String
             return string.IsNullOrEmpty(end) ? str[indexOfStart..] : str[indexOfStart..indexOfEnd];
         }
 
-        public static bool Contains(this string str, string toCheck, StringComparison comp)
+        public static T To<T>(this string str)  //TODO: more types, more checks
         {
             if (!str.Valid())
                 throw new InvalidOperationException("Input string in wrong format!");
 
-            return str.IndexOf(toCheck, comp) >= 0;
+            switch (typeof(T))
+            {
+                case Type intType when intType == typeof(int):
+                    {
+                        if (!str.IsDigitOnly() && !str.Contains("-"))
+                            throw new InvalidOperationException("Input string in wrong format, non-digit char other then '-' present.");
+                        if (str.ToCharArray().Length > 10)
+                            throw new InvalidOperationException("Input string in wrong format, too many digits maximum for int32 is 10.");
+
+                        int value;
+                        try {
+                            value = int.Parse(str);
+                        }
+                        catch {
+                            throw;
+                        }
+                            
+                        return (T) Convert.ChangeType(value, typeof(T));
+                    }
+                case Type longType when longType == typeof(long):
+                    {
+                        if (!str.IsDigitOnly() && !str.Contains("-"))
+                            throw new InvalidOperationException("Input string in wrong format, non-digit char other then '-' present.");
+                        if (str.ToCharArray().Length > 19)
+                            throw new InvalidOperationException("Input string in wrong format, too many digits maximum for int32 is 10.");
+
+                        long value;
+                        try {
+                            value = long.Parse(str);
+                        }
+                        catch {
+                            throw;
+                        }
+
+                        return (T) Convert.ChangeType(value, typeof(T));
+                    }
+                case Type floatType when floatType == typeof(float):
+                    {
+                        if (!str.IsDigitOnly() && !str.ContainsSpecialDigitChar())
+                            throw new InvalidOperationException("Input string in wrong format, non-digit char other then '-' present.");
+                        if (str.ToCharArray().Length > 9)
+                            throw new InvalidOperationException("Input string in wrong format, too many digits maximum for int32 is 10.");
+                      
+                        float value;
+                        try {
+                            value = float.Parse(str);
+                        }
+                        catch {
+                            throw;
+                        }
+
+                        return (T) Convert.ChangeType(value, typeof(T));
+                    }
+                case Type doubleType when doubleType == typeof(double):
+                    {
+                        if (!str.IsDigitOnly() && !str.ContainsSpecialDigitChar())
+                            throw new InvalidOperationException("Input string in wrong format, non-digit char other then '-' present.");
+                        if (str.ToCharArray().Length > 17)
+                            throw new InvalidOperationException("Input string in wrong format, too many digits maximum for int32 is 10.");
+
+                        double value;
+                        try
+                        {
+                            value = double.Parse(str);
+                        }
+                        catch
+                        {
+                            throw;
+                        }
+
+                        return (T) Convert.ChangeType(value, typeof(T));
+                    }
+                case Type decimalType when decimalType == typeof(decimal):
+                    {
+                        if (!str.IsDigitOnly() && !str.ContainsSpecialDigitChar())
+                            throw new InvalidOperationException("Input string in wrong format, non-digit char other then '-' present.");
+                        if (str.ToCharArray().Length > 29)
+                            throw new InvalidOperationException("Input string in wrong format, too many digits maximum for int32 is 10.");
+
+                        decimal value;
+                        try
+                        {
+                            value = decimal.Parse(str);
+                        }
+                        catch
+                        {
+                            throw;
+                        }
+
+                        return (T) Convert.ChangeType(value, typeof(T));
+                    }
+
+                default:
+                    throw new InvalidOperationException("Input type is not supported!");
+            }
         }
     }
 }
